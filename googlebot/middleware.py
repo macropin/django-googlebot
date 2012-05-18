c435e073a1bea2020257ad6e69c297711571db9d
Andrew Cutler <macropin@gmail.com>
2012-05-18 21:47:09 +1000

    Bugfix and add comment

--- googlebot/middleware.py
+++ googlebot/middleware.py
@@ -5,17 +5,19 @@ from django.contrib.auth.models import User
 
 
 class GooglebotMiddleware(object):
-
+    """
+    Middleware to automatically log in the Googlebot with the user account 'googlebot'
+    """
     def process_request(self, request):
+        request.is_googlebot = False # Assume false, until proven
         if request.user == AnonymousUser():
-            request.is_googlebot = False # Assume false, until proven
             if 'Googlebot' in request.META['HTTP_USER_AGENT']:
                 try:
                     remote_ip = request.META['REMOTE_ADDR']
-                    hostname, aliaslist, ipaddrlist = str(socket.gethostbyaddr(remote_ip))
+                    hostname, aliaslist, ipaddrlist = socket.gethostbyaddr(remote_ip)
 
-                    if hostname.endswith('googlebot.com.'):
-                        request.user = User.objects.get(username='googlebot') # login our googlebot user :)
+                    if hostname.endswith('googlebot.com'):
+                        request.user = User.objects.get_or_create(username='googlebot') # login our googlebot user :)
                         request.is_googlebot = True
                     else:
                         # FAKE googlebot!!!!
