e947a6663222d0b64983ebda4c22d7755bf93e73
Andrew Cutler <macropin@gmail.com>
2012-05-18 17:04:53 +1000

    Setup added

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='googlebot',
+    version='0.0.1',
+    url='https://github.com/macropin/django-googlebot',
+    packages=['googlebot',],
+)
